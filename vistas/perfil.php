153b91a279142122ceb100fab21adb1b0b9b1ad7
Days-hug <161251337+DayElvir@users.noreply.github.com>
2024-03-21 17:23:01 -0600

    Update perfil.php

--- vistas/perfil.php
+++ vistas/perfil.php
@@ -90,33 +90,9 @@ $result = $conexion->query($sql);
 }*/
 
 
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <link rel="stylesheet" href="../assets/css/perfil.css">
-  <title>Perfil</title>
-</head>
-<body>
-
-
-
   
   <div class="container emp-profile">
-
-
     <form method="post">
-
-
         <div class="row">
             <div class="col-md-4">
                 <div class="profile-img">
@@ -278,5 +254,3 @@ $result = $conexion->query($sql);
         }
     });
 </script>
-</body>
-</html>
